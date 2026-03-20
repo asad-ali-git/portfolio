f8f6b77550494a03c40108cd26cbd0cf7ad8c91d
Asad Ali <asadali.boson@gmail.com>
2026-03-21 01:25:21 +0500

    feat: add route to clear application cache

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'app');
 
 Route::view('/{any}', 'app')
     ->where('any', '^(?!api|admin|filament).*$');
+
+Route::get('/clear', function () {
+    Artisan::call('optimize:clear');
+    return 'Cleared';
+});
