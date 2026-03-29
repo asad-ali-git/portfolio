45b1b1b35629f0005b7577ef722f1300c31a82ce
Asad Ali <asadali.boson@gmail.com>
2026-03-30 00:01:32 +0500

    feat: log inspiring quote display in console command

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
+    Log::info('Inspiring quote displayed: ' . Inspiring::quote());
 })->purpose('Display an inspiring quote');
