4bcf21d59996fe8f644dd1249cb9f3b646faadf7
Asad Ali <asadali.boson@gmail.com>
2026-03-30 23:46:02 +0500

    feat: update route to exclude 'clear' from wildcard matching

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'app');
 
-Route::view('/{any}', 'app')
-    ->where('any', '^(?!api|admin|filament).*$');
-
 Route::get('/clear', function () {
     Artisan::call('optimize:clear');
     return 'Cleared';
 });
+
+Route::view('/{any}', 'app')
+    ->where('any', '^(?!(api|admin|filament|clear)$).*$');
+
